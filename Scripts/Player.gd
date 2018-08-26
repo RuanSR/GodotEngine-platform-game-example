8e2d38225fcc68159b3f543dd687a01ad01238d4
RuanSR <ruansilvarosa@gmail.com>
2018-08-26 17:06:31 -0300

    2 - Criado a Cena 'Wold', Criado script para movimentar o player

---
+++ Scripts/Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+const UP = Vector2(0,-1)
+const GRAVITY = 20
+const SPEED = 100
+const JUMP_HEIGHT = -500
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.y = GRAVITY
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+	else:
+		motion.x = 0
+	
+	if is_on_floor():
+		motion.y = JUMP_HEIGHT
+		
+	motion = move_and_slide(motion,UP)#Move o player passando um Vector2
